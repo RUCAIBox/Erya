42eab6235854fa948c69f2cfe77b9dcbac6d4346
cherryguo <88224039+Guochry@users.noreply.github.com>
2023-07-27 22:54:43 +0800

    Delete textbox directory

--- textbox/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from textbox.utils.logger import init_logger
-from textbox.utils.utils import get_local_time, ensure_dir, get_model, get_trainer, init_seed, serialized_save, safe_remove
-from textbox.utils.argument_list import general_parameters, training_parameters, evaluation_parameters
